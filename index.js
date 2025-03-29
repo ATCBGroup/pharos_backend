d5bf8803b69b787906c669e9ee3dbf274c91a9ce
ATCBGroup <97746383+ATCBGroup@users.noreply.github.com>
2025-03-29 09:22:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ app.get("/", (req, res) => {
   res.send("Hello from Node.js GitHub Versions!");
 });
 
+// The HelloWorld
+app.get("/calendar", (req, res) => {
+  const calendar = [
+    { id: 1, name: "Calendar 1" },
+    { id: 2, name: "Calendar 2" },
+  ];
+  res.json(calendar);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App listening on http://localhost:${port}`);
