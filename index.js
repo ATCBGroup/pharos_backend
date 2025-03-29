da973dc58e34da17da8ff42eee85765d791c41f8
ATCBGroup <97746383+ATCBGroup@users.noreply.github.com>
2025-03-29 09:30:59 +0100

    add sqlite3

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+var sqlite3 = require("sqlite3").verbose();
 
 // The HelloWorld
 app.get("/", (req, res) => {
@@ -8,11 +9,28 @@ app.get("/", (req, res) => {
 
 // The HelloWorld
 app.get("/calendar", (req, res) => {
-  const calendar = [
-    { id: 1, name: "Calendar 1" },
-    { id: 2, name: "Calendar 2" },
-  ];
+  const db = new sqlite3.Database("calendar");
+
+  db.all("SELECT * FROM calendar", [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    res.json(rows);
+  });
+
   res.json(calendar);
+});
+
+app.get("/create", (req, res) => {
+  const db = new sqlite3.Database("calendar");
+  db.serialize(() => {
+    db.run("CREATE TABLE calendar (id INT, name TEXT)");
+    const stmt = db.prepare("INSERT INTO calendar VALUES (?, ?)");
+    stmt.run(1, "Calendar 1");
+    stmt.finalize();
+  });
+  db.close();
+  res.send("Calendar created!");
 });
 
 const port = process.env.PORT || 3000;
