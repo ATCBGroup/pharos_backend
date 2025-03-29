b0e18ccb477e2e94deb78a4da922c151d9c50077
ATCBGroup <97746383+ATCBGroup@users.noreply.github.com>
2025-03-29 15:41:04 +0100

    up

--- index.js
+++ index.js
@@ -1,35 +1,69 @@
 const express = require("express");
+
+const db = require("better-sqlite3")("pharos.db");
 const app = express();
-var sqlite3 = require("sqlite3").verbose();
 
 // The HelloWorld
 app.get("/", (req, res) => {
   res.send("Hello from Node.js GitHub Versions!");
 });
 
-// The HelloWorld
-app.get("/calendar", (req, res) => {
-  const db = new sqlite3.Database("calendar.db");
+app.get("/init", (req, res) => {
+  // Initialize the database and create the table if it doesn't exist
 
-  db.all("SELECT * FROM test", [], (err, rows) => {
-    if (err) {
-      throw err;
-    }
-    res.json(rows);
+  const query = `
+  CREATE TABLE calendar (
+    id INTEGER PRIMARY KEY, 
+    nameEvent STRING NOT NULL,
+    description STRING NOT NULL,
+    startDate STRING NOT NULL,
+    endDate STRING NOT NULL,
+    timeline INTEGER
+
+)`;
+
+  db.exec(query);
+
+  const data = [
+    { nameEvent: "Event 1", description: "Description 1", startDate: "2023-10-01", endDate: "2023-10-02", timeline: 1 },
+    { nameEvent: "Event 2", description: "Description 2", startDate: "2023-10-03", endDate: "2023-10-04", timeline: 2 },
+    { nameEvent: "Event 3", description: "Description 3", startDate: "2023-10-05", endDate: "2023-10-06", timeline: 3 },
+    { nameEvent: "Event 4", description: "Description 4", startDate: "2023-10-07", endDate: "2023-10-08", timeline: 4 },
+  ];
+
+  const inserData = db.prepare(`INSERT INTO calendar (nameEvent, description, startDate, endDate, timeline) VALUES (?, ?, ?, ?, ?)`);
+  data.forEach((element) => {
+    inserData.run(element.nameEvent, element.description, element.startDate, element.endDate, element.timeline);
   });
+
+  res.send("Database initialized and data inserted!");
 });
 
-app.get("/write", (req, res) => {
-  const db = new sqlite3.Database("calendar.db");
+app.get("/calendar", (req, res) => {
+  try {
+    const query = `
+        SELECT * FROM calendar
+      `;
 
-  db.run("INSERT INTO test (id, name) VALUES (?, ?)", ["2", "deux"], function (err) {
-    if (err) {
-      return console.log(err.message);
-    }
-    res.json({
-      message: "A row has been inserted",
-    });
-  });
+    const stmt = db.prepare(query);
+    const calendar = stmt.all();
+
+    res.json(calendar);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Please init database first" });
+  }
+});
+
+app.post("/calendar", (req, res) => {
+  const { nameEvent, description, startDate, endDate, timeline } = req.body;
+
+  // Insert new user into the database
+  const insertQuery = `INSERT INTO calendar (nameEvent, description, startDate, endDate, timeline) VALUES (?, ?, ?, ?, ?)`;
+  const stmt = db.prepare(insertQuery);
+  stmt.run(nameEvent, description, startDate, endDate, timeline);
+
+  res.status(201).json({ message: "Event added successfully!" });
 });
 
 const port = process.env.PORT || 3000;
